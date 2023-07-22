8e6ba020b14c778b05ccadaf8598f60e47e42acd
touch <tttouch1122@gmail.com>
2023-07-22 17:34:20 +0700

    2

--- src/app/page.js
+++ src/app/page.js
@@ -32,6 +32,9 @@ export default function Home() {
     setTasks(newTasks);
   };
 
+  const allCount = tasks.length;
+  const doneCount = tasks.filter((task) => task.completed).length;
+
   return (
     // Main container
     <div className="container mx-auto">
@@ -41,7 +44,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All (allCount) Done (doneCount)
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
