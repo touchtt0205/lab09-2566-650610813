2e076005cdf31b92557f8d3e894cd8e0c0f0e6fe
touch <tttouch1122@gmail.com>
2023-07-22 17:29:15 +0700

    commit

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -15,14 +15,15 @@ export const Task = ({
 
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-      {/*
-      HINTS: if task is completed, below "span" will show like this 
-        <span className="text-decoration-line-through">{title}</span>
-        But if task is not completed : 
-        <span>{title}</span>
-      */}
+      {completed ? (
+          <span className="text-decoration-line-through">{title}</span>
+        ) : (
+          <span>{title}</span>
+        )
+        }
       <span>{title}</span>
-      <button className="btn btn-success">Done</button>
+      <button className="btn btn-success" onClick={toggleBtnOnClick}>
+        Done</button>
       <button className="btn btn-danger" onClick={deleteBtnOnClick}>
         Delete
       </button>
